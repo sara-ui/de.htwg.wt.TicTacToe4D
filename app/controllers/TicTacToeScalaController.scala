55057462debdc0b8834733278caa86e9ed116af9
Sara Maanni <sara.maanni@uni-konstanz.de>
2020-10-21 23:22:39 +0200

    Task 1: Play Server - creating different routes: /players.., /move/step, /restart, /reset

--- app/controllers/TicTacToeScalaController.scala
+++ app/controllers/TicTacToeScalaController.scala
@@ -7,14 +7,64 @@ import de.htwg.se.ticTacToe3D.TicTacToe
 @Singleton
 class TicTacToeScalaController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gameController = TicTacToe.controller
-  def tictactoAsText =  gameController.toString + gameController.statusMessage
+  def tictactoeAsText =  gameController.toString + gameController.statusMessage
 
+  /**
+   * the About Page
+   * @return the about page
+   */
   def about= Action {
     Ok(views.html.index())
   }
 
+  /**
+   * TUI Page
+   * @return the TUI + Status message
+   */
   def tictactoe = Action {
-    Ok(tictactoAsText)
+    Ok(tictactoeAsText)
+  }
+
+  /**
+   * Setting players names
+   * @param player1 name
+   * @param player2 name
+   * @return TUI + status message
+   */
+  def players(player1: String, player2: String) = Action {
+    gameController.setPlayers(player1, player2)
+    Ok(tictactoeAsText)
+  }
+
+  /**
+   * Making a game move
+   * @param step move
+   * @return TUI + status message
+   */
+  def move(step: String) = Action { // "123" --> ["1", "2", "3"] --> [1, 2, 3]
+    step.toList.filter(c => c != ' ').map(c => c.toString.toInt) match {
+      case row :: column :: grid :: Nil => gameController.setValue(row, column, grid)
+      case _ => None
+    }
+    Ok(tictactoeAsText)
+  }
+
+  /**
+   * reset the game --> reset players
+   * @return TUI + status message
+   */
+  def reset = Action {
+    gameController.reset
+    Ok(tictactoeAsText)
+  }
+
+  /**
+   * restart the game
+   * @return TUI + status message
+   */
+  def restart = Action {
+    gameController.restart
+    Ok(tictactoeAsText)
   }
 
 }
