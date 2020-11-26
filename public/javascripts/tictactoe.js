3493cc7ca786109eff0413967432bfa7d8901648
Sara Maanni <sara.maanni@uni-konstanz.de>
2020-11-26 08:58:56 +0100

    WT-06-JS: implementing js / json / jquery / ajax --> adding comments

--- public/javascripts/tictactoe.js
+++ public/javascripts/tictactoe.js
@@ -1,15 +1,30 @@
+/**
+ * The default size of the Game grids
+ * @type {number}
+ */
 let size = 4
 
 class Game {
+    /**
+     * the Constructor gets the size of the game
+     * @param size of the game
+     */
     constructor(size) {
         this.size = size
         this.grids = []
     }
 
+    /**
+     * Fills the grids array with the values json
+     * @param json the new values of the grids
+     */
     fill(json) {
         this.grids = json
     }
 
+    /**
+     * Creates the new html grids programmatically
+     */
     updateView() {
         let html = ""
         for (let grid = 0; grid < this.grids.length; grid++) {
@@ -30,6 +45,9 @@ class Game {
         }
     }
 
+    /**
+     * registers the click listener for every single cell
+     */
     registerClickListener() {
         for (let grid = 0; grid < this.grids.length; grid++) {
             for (let row = 0; row < this.grids[grid].length; row++) {
@@ -44,6 +62,12 @@ class Game {
         }
     }
 
+    /**
+     * Creates the HTML for the status message
+     * @param message the new status message
+     * @param won boolean that indicates if the games is already finished
+     * @returns {string}
+     */
     statusMessage(message, won) {
         return `<div class="statusMessage__header alert alert-success">
                     <span class="statusMessage__header__header">
@@ -54,10 +78,21 @@ class Game {
                 </div>`
     }
 
+    /**
+     * Updates the HTML using jQuery
+     * @param message the new status message
+     * @param won boolean that indicates if the games is already finished
+     */
     updateStatusMessage(message, won) {
         $("#statusMessage").html(this.statusMessage(message, won))
     }
 
+    /**
+     * Sends the move values to the BE using ajax and update the Grid that has been clicked
+     * @param grid index of the grid
+     * @param row the row in the grid
+     * @param column the column in the grid
+     */
     move(grid, row, column) {
         $.ajax({
             method: "POST",
@@ -80,12 +115,17 @@ class Game {
         });
     }
 
-    updateGrid(grid, result) {
-        this.grids[grid] = result
-        for (let row = 0; row < result.length; row++) {
-            for (let column = 0; column < result[row].length; column++) {
-                if (result[row][column] !== '-') {
-                    const player = result[row][column] === 'X' ? 1 : 2
+    /**
+     * Update the Grid cells and make them not clickable if they already have been clicked before
+     * @param grid index of the grid
+     * @param newGrid the new grid from the BE
+     */
+    updateGrid(grid, newGrid) {
+        this.grids[grid] = newGrid
+        for (let row = 0; row < newGrid.length; row++) {
+            for (let column = 0; column < newGrid[row].length; column++) {
+                if (newGrid[row][column] !== '-') {
+                    const player = newGrid[row][column] === 'X' ? 1 : 2
                     $('#notSet' + grid + '-' + row + '-' + column).addClass('col col__player' + player).removeAttr('id');
                 }
             }
@@ -95,6 +135,9 @@ class Game {
 
 let game = new Game(4)
 
+/**
+ * Load the game for the first time from the BE to keep data preserved
+ */
 function loadGame() {
     $.ajax({
         method: "GET",
@@ -112,6 +155,9 @@ function loadGame() {
     });
 }
 
+/**
+ * When the document is ready load the game
+ */
 $(document).ready(function () {
     console.log("start loading our Game")
     loadGame();
