bd373c2202ea72123077c160022e20c6b2b9ffee
Andre Göhring <a-goehring@web.de>
2021-01-26 15:35:35 +0100

    Pushing Vue stuff

---
+++ public/myAssets/js/about.54d0f2df.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},c=[],u={name:"About"},o=u,r=t("2877"),s=Object(r["a"])(o,a,c,!1,null,"ac26ea5c",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=about.54d0f2df.js.map
---
+++ public/myAssets/js/game.0e3eb836.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["game"],{"7d36":function(e,n,a){"use strict";a.r(n);var t=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("div",[e._v("game")])},c=[],r={name:"Game"},s=r,u=a("2877"),i=Object(u["a"])(s,t,c,!1,null,"7cb5a190",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=game.0e3eb836.js.map
