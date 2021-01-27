da9de119fceb81516d16f39b8d56cfadcd688616
Andre Göhring <a-goehring@web.de>
2021-01-27 11:46:02 +0100

    Fixing Overflow Issue

--- frontend/tictactoe-vue/src/App.vue
+++ frontend/tictactoe-vue/src/App.vue
@@ -146,6 +146,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  overflow: hidden;
 }
 
 #nav {
@@ -164,5 +165,6 @@ html,
 body {
   width: 100%;
   height: 100%;
+  overflow: hidden;
 }
 </style>
